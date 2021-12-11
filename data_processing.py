b8163c96561e577c4000003b7198fdcda6208c78
mattkus13 <89753800+mattkus13@users.noreply.github.com>
2021-12-11 15:03:52 -0800

    Added compatibility for larger data sets, reformatted output, added report document

--- data_processing.py
+++ data_processing.py
@@ -4,13 +4,17 @@
 #   Derived from DataFlair's implementation found at:
 #   https://data-flair.training/blogs/python-mini-project-speech-emotion-recognition/
 
+from genericpath import isdir
+import time
 import librosa
+from numpy.core.numeric import full
 import soundfile
-import os, glob, pickle
+import os, glob, pickle, sys
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
+from pydub import AudioSegment
 
 # Globals:
 data_dir = './ravdess/'
@@ -24,8 +28,11 @@ emotions ={
   '07':'disgust',
   '08':'surprised'
 } 
+size_read = 0
+observed_emotions=['calm', 'happy', 'fearful', 'disgust']
+skipped_files = []
 
-    # TODO:
+    # **********************Approach**************************
     # Create function to extract mfcc, chroma, and mel features from files
     #   use soundfile to open/read
     #   return result as numpy array
@@ -48,14 +55,41 @@ emotions ={
 def load_data():
     x = []
     y = []
-    for filename in glob.glob(data_dir + '/Actor_*/*.wav'):
-        y.append(filename.split('-')[2])
-        x.append(extract_feature(filename, True, True, True))
+    full_dir = data_dir + '/Actor_*/*.wav'
+    for filename in glob.glob(full_dir):
+        print('Loading file:  %s' % filename)
+
+        size = os.path.getsize(filename)
+        print('Size: %.2f' % size)
+        global size_read
+        size_read += size
+
+        feature = extract_feature(filename, True, True, True)
+
+        if not isinstance(feature, type(None)):
+            emotion=emotions[filename.split("-")[2]]
+            if emotion not in observed_emotions:
+                continue
+            
+            x.append(feature)
+            y.append(filename.split('-')[2])
+        else:
+            print('%s not mono audio, skipping' % filename)
+            global skipped_files
+            skipped_files.append(filename)
+            size_read -= size
+
     return x, y
 
 # Extracts the 3 key features from audio file
 def extract_feature(file_name, mfcc, chroma, mel):
+    # test = AudioSegment.from_wav(file_name)
+    # test.set_channels(1)
+    # test.export(file_name)
     with soundfile.SoundFile(file_name) as sound_file:
+        if sound_file.channels > 1:
+            return None
+
         X = sound_file.read(dtype='float32')
         sample_rate = sound_file.samplerate
 
@@ -73,13 +107,33 @@ def extract_feature(file_name, mfcc, chroma, mel):
             result=np.hstack((result, mel))
     return result
 
+# Start timer
+start_time = time.time()
+
+# If path specified
+if len(sys.argv) > 1:
+    if os.path.isdir(sys.argv[1]):
+        data_dir = sys.argv[1]
+    else:
+        print('%s is invalid directory' % sys.argv[1])
+        sys.exit()
+
 x,y = load_data()
 data = train_test_split(x, y, test_size=.25)
 xtrain, xtest, ytrain, ytest = data
-model=MLPClassifier(alpha=0.01, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500, shuffle=True)
+
+print('\nCreating classifier...')
+model=MLPClassifier(alpha=0.01, batch_size=256, epsilon=1e-08, hidden_layer_sizes=(300,), learning_rate='adaptive', max_iter=500)
 model.fit(xtrain, ytrain)
+print('\nPredicting using fit model...')
 predicted = model.predict(xtest)
 accuracy = accuracy_score(ytest, predicted)
 
-print(accuracy)
-print('Well that went swimmingly!')
+print('\nAccuracy: {:.2f}%'.format(round(accuracy*100, 2)))
+
+# End time, calculate duration
+end_time = time.time()
+time_elapsed = time.strftime('%H:%M:%S', time.gmtime(end_time-start_time))
+
+print('\nRead {:.2f} MB in {:s}'.format(size_read/1048576, time_elapsed))
+print('Skipped {:d} files'.format(len(skipped_files)))
